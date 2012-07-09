9e051ae44c531e66d6d37a5f562699113e722fb2
guillermooo <guilan70@hotmail.com>
2012-07-10 00:53:26 +0200

    add registers class to manage vim registers

---
+++ registers.py
@@ -0,0 +1,83 @@
+import sublime
+import os
+
+# todo(guillermo): some operations will require a view. fix that.
+class Registers(dict):
+    """
+    Registers hold global data mainly used by yank, delete and paste.
+    Create only one instance of this class.
+    """
+    REG_DEFAULT = '"'
+    REG_SMALL_DELETE = '-'
+    REG_NULL = '_'
+    REG_LAST_INSERTED_TEXT = '.'
+    REG_FILE_NAME = '%'
+    REG_ALT_FILE_NAME = '#'
+    REG_ALL = (REG_DEFAULT, REG_SMALL_DELETE, REG_NULL, REG_LAST_INSERTED_TEXT,
+               REG_FILE_NAME, REG_ALT_FILE_NAME)
+    # todo(guillermo): there are more
+
+    def _set_default_register(self, value):
+        # todo(guillermo): could be made a decorator.
+        self.__dict__[Registers.REG_DEFAULT] = value
+
+    def set(self, name, value):
+        """
+        Sets an a-z or 0-9 register.
+        """
+        assert len(name) == 1, "Register names must be 1 char long."
+
+        if isinstance(name, int):
+            name = unicode(name)
+        if (not (name.isalpha() or name.isdigit())) or name.isupper():
+                # Vim fails silently.
+                # raise Exception("Can only set a-z and 0-9 registers.")
+                return None
+        self.__dict__[name] = value
+        self._set_default_register(value)
+
+    def append_to(self, name, value):
+        """
+        Appends to an a-z register. `name` must be a capital in A-Z.
+        """
+        assert len(name) == 1, "Register names must be 1 char long."
+
+        if isinstance(name, int):
+            name = unicode(name)
+        if name.islower() or not name.isalpha():
+            # todo(guillermo): maybe fail silently?
+            raise Exception("Can only append to A-Z registers.")
+        existing = self.__dict__.get(name.lower(), '')
+        self.__dict__[name.lower()] = existing + value
+        self._set_default_register(self[name.lower()])
+
+    def get(self, name):
+        assert len(name) == 1, "Register names must be 1 char long."
+
+        if name == Registers.REG_NULL:
+            return
+        elif name == Registers.REG_FILE_NAME:
+            try:
+                # todo(guillermo): there must be a better way of doing this.
+                v = sublime.active_window().active_view().file_name()
+                return os.path.basename(v)
+            except AttributeError:
+                return ''
+        elif name != Registers.REG_DEFAULT and name in Registers.REG_ALL:
+            return None
+
+        try:
+            return self.__dict__[name.lower()]
+        except KeyError:
+            # sublime.status_message("Vintage.Next: E353 Nothing in register %s", name)
+            pass
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        try:
+            if key.isupper():
+                self.append_to(key, value)
+        except AttributeError:
+            self.set(key, value)
